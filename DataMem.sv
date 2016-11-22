5e49f0cf75780581160cb5e6b060bf50593aa45b
panwangc <panwangc@uci.edu>
2016-11-22 14:48:11 -0800

    Add files via upload

---
+++ DataMem.sv
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/21/2016 10:50:50 PM
+// Design Name: 
+// Module Name: DataMem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module DataMem(
+    input logic         clk,
+    input logic [31:0]   Address,
+    input logic [31:0]   Write_Data,
+    input logic         WE2,
+    output logic [31:0] Read_Data
+    //output logic        out
+    );
+    
+    logic [31:0] mem [128:0];
+    
+    always_ff @(posedge clk) begin
+        if (WE2) begin
+            mem[Address] <= Write_Data;
+        end
+        
+        else begin
+            Read_Data <= mem[Address];
+        end
+    end
+    //assign out = Read_Data;
+endmodule
---
+++ MUXsel2.sv
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/22/2016 02:30:32 PM
+// Design Name: 
+// Module Name: MUXsel2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MUXsel2(
+    input logic [31:0]  Read_Data,
+    input logic [31:0]  ALUresult,
+    input logic [1:0]   MUXsel2,
+    output logic[31:0]  Out_Data
+    );
+    assign Out_Data = (MUXsel2 == 1'b1)?ALUresult:Read_Data;
+endmodule

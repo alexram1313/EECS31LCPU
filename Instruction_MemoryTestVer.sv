9019da817268bb271f22baddf643b39e790907b3
cihaddad <cihaddad@uci.edu>
2016-11-23 15:29:35 -0800

    Add files via upload

---
+++ Instruction_MemoryTestVer.sv
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/22/2016 04:33:39 PM
+// Design Name: 
+// Module Name: Instruction_Memory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Instruction_Memory(
+    input logic         clk,
+    input logic reset,
+    input logic [5:0]   address,
+    output logic [31:0] out_data
+    );
+    
+    logic [63:0] mem [31:0];
+    
+    always_ff @(posedge clk) begin
+        //out_data <= mem[address];
+           if (!reset) begin
+                out_data <= mem[address];
+           end
+           else begin
+                mem <= '{64{0}};
+                end
+    end
+endmodule
---
+++ muxTestVer.sv
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/22/2016 02:30:32 PM
+// Design Name: 
+// Module Name: MUXsel2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Mux(
+    input logic [31:0]  op1,
+    input logic [31:0]  op2,
+    input logic [1:0]   sel,
+    output logic[31:0]  out_data
+    );
+    assign out_data = (sel == 1'b1)?op1:op2;
+endmodule

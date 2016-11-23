6332d2ffa072171cb22ae7a1ea376399b7601443
cihaddad <cihaddad@uci.edu>
2016-11-22 16:21:58 -0800

    Update Controller.sv

--- Controller.sv
+++ Controller.sv
@@ -39,14 +39,14 @@ module Controller(
     //logic = instructionmem(inst);
     
     assign mux_sel1 = (instruction[0] == 1'b0)? 0:1;  //if LSB of instruction is 0, its a register type and selector set to 0; else 1 signifying immediate type
-    assign mux_sel2 = (instruction[15:12] == 4'b0100 | instruction[15:12] == 4'b0110)? 1:0; //if read/store memory is called, muxsel2 is set to 1; else taking alu value
-    assign regwrite = (instruction[15:12] != 4'b0110)? 1:0;
-    assign memwrite = (instruction[15:12] == 4'b0110)? 1:0;
+    assign mux_sel2 = (instruction[16:13] == 4'b0100 | instruction[15:12] == 4'b0110)? 1:0; //if read/store memory is called, muxsel2 is set to 1; else taking alu value
+    assign regwrite = (instruction[16:13] != 4'b0110)? 1:0;
+    assign memwrite = (instruction[16:13] == 4'b0110)? 1:0;
     
     assign alu_mode = instruction[12];
-    assign alu_opsel = instruction[15:13];
+    assign alu_opsel = instruction[16:14];
     assign rs = instruction[6:1];
-    assign rt = instruction[23:18];
+    assign rt = instruction[22:17];
     assign rd = instruction[12:7];
     assign imm = instruction[31:17];
     
